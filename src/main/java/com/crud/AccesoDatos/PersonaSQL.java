11b41b98fcc1462fcdbfbea3909a0909ad575b66
Adrian Berrelleza <adrianberrelleza4@gmail.com>
2026-08-15 19:59:57 -0700

    Avances de las calses donde se realizaran las operaciones sql

--- src/main/java/com/crud/AccesoDatos/PersonaSQL.java
+++ src/main/java/com/crud/AccesoDatos/PersonaSQL.java
@@ -1,4 +1,50 @@
 package com.crud.AccesoDatos;
 
+import com.crud.Conexion.ConectDB;
+import com.crud.Logica.Persona;
+
+import java.sql.*;
+
 public class PersonaSQL {
+
+    public boolean insertarPersona(Persona persona) {
+        String sql = "INSERT INTO Personas (nombre, direccion) VALUES (? , ?)";
+        try (Connection conn = ConectDB.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+
+            ps.setString(1, persona.getNombre());
+            ps.setString(2, persona.getDireccion());
+            int filas = ps.executeUpdate();
+
+            try(ResultSet rs = ps.getGeneratedKeys()) {
+                if (rs.next()) {
+                    int id = rs.getInt(1);
+                    persona.setId(id);
+                }
+            }
+            System.out.println("Filas: " + filas);
+            return filas >0;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean borrarPersona(int id) {
+        String sql = "DELETE FROM Personas WHERE idPersona = ?";
+        try(Connection conn = ConectDB.getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)){
+
+            ps.setInt(1, id);
+            int filas = ps.executeUpdate();
+            System.out.println("Filas: " + filas);
+            return filas > 0;
+
+        }catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+
 }
--- src/main/java/com/crud/Conexion/ConectDB.java
+++ src/main/java/com/crud/Conexion/ConectDB.java
@@ -1,4 +1,16 @@
 package com.crud.Conexion;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConectDB {
+
+    private static final String url = "jdbc:mysql://localhost:3306/crud";
+    private static final String user = "usuario1";
+    private static final String password = "superpassword";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url,user,password);
+    }
 }
