5821f2415a5417e481f0b71d7373c50fa955eb8a
Adrian Berrelleza <adrianberrelleza4@gmail.com>
2026-08-15 17:53:23 -0700

    Primera version de la Clases Base de Persona y Telefono

--- src/main/java/com/crud/Logica/Persona.java
+++ src/main/java/com/crud/Logica/Persona.java
@@ -1,4 +1,47 @@
 package com.crud.Logica;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Persona {
+    private String nombre;
+    private int id;
+    private String direccion;
+    private List<Telefono> telefonos;
+
+    public Persona(String nombre, int id, String direccion) {
+        this.nombre = nombre;
+        this.id = id;
+        this.direccion = direccion;
+        telefonos = new ArrayList<>();
+    }
+
+    public Persona() {
+        telefonos = new ArrayList<>();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getDireccion() {
+        return direccion;
+    }
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    public List<Telefono> getTelefonos() {
+        return telefonos;
+    }
+    public void addTelefono(Telefono telefono) {
+        this.telefonos.add(telefono);
+    }
 }
--- src/main/java/com/crud/Logica/Telefono.java
+++ src/main/java/com/crud/Logica/Telefono.java
@@ -1,4 +1,35 @@
 package com.crud.Logica;
 
 public class Telefono {
+    private int id;
+    private String numero;
+    private int personaId;
+
+    public Telefono() {
+
+    }
+    public Telefono(int id, String numero, int personId) {
+        this.id = id;
+        this.numero = numero;
+        this.personaId = personId;
+
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNumero() {
+        return numero;
+    }
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+    public int getPersonId() {
+        return personaId;
+    }
+    public void setPersonId(int personId) {
+        this.personaId = personId;
+    }
 }
